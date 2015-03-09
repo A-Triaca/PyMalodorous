0b1f96a8e68183050ce947f80a5bb84bdb845053
A-Triaca <alex.triaca@gmail.com>
2015-03-09 21:56:58 +0200

    Added DB link and base password loop.

--- Malodorous.py
+++ Malodorous.py
@@ -1,7 +1,23 @@
 __author__ = 'Alex'
 
+import pyodbc
+import datetime
+
 def main():
-    print("Not yet implemented")
+    ##Setup connection to SQL Server
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=Malodorous;UID=sa;PWD=password')
+    cursor = cnxn.cursor()
+    ##Open training password file
+    passwordFile = open('10kMostCommon.txt', 'r')
+    ##Loop through passwords in file and break them down and add to DB
+    for password in passwordFile:
+        ##Insert password into DB and commit
+        cursor.execute("INSERT INTO Password VALUES (" + password + ", " + password.__len__() + ", " + datetime.datetime + ", " + False + ");")
+        cnxn.commit()
+        ##Get the inserted password ID
+        passordId = cursor.execute("SELECT @@IDENTITY")
+        print(passordId)
+
 
 if __name__ == "__main__":
     main()
