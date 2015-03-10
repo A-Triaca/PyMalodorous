9734ee496d4942a992639120354cbaf7424724cd
A-Triaca <alex.triaca@gmail.com>
2015-03-10 22:09:01 +0200

    Fixed DB connection and tested Password insert.

--- Malodorous.py
+++ Malodorous.py
@@ -11,12 +11,14 @@ def main():
     passwordFile = open('10kMostCommon.txt', 'r')
     ##Loop through passwords in file and break them down and add to DB
     for password in passwordFile:
+        ##Trim password
+        password = password.strip()
         ##Insert password into DB and commit
-        cursor.execute("INSERT INTO Password VALUES (" + password + ", " + password.__len__() + ", " + datetime.datetime + ", " + False + ");")
+        ######ESCAPE SPECIAL CHARACTERS
+        cursor.execute("INSERT INTO dbo.Password (Password, Length, DateAdded, Deleetified) VALUES ('" + password + "', " + str(password.__len__()) + ", '" + str(datetime.datetime.now()) + "', '" + str(False) + "');")
         cnxn.commit()
         ##Get the inserted password ID
-        passordId = cursor.execute("SELECT @@IDENTITY")
-        print(passordId)
+        #passordId = cursor.execute("SELECT @@IDENTITY").fetchone()[0]
 
 
 if __name__ == "__main__":
