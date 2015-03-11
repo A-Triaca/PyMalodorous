b32018e33e14ddaf767df497143fca766a53df37
A-Triaca <alex.triaca@gmail.com>
2015-03-11 20:54:57 +0200

    Escaped single quotes for SQL.

--- Malodorous.py
+++ Malodorous.py
@@ -4,31 +4,34 @@ import pyodbc
 import datetime
 
 def passwordAdvancedMask(password):
-    lowerChars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', \
-                 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', \
-                 'u', 'v', 'w', 'x', 'y', 'z'}
-    upperChars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', \
-                 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', \
-                 'U', 'V', 'W', 'X', 'Y', 'Z'}
-    numbers = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
 
     advancedMask = ""
 
     for i in range(password.__len__()):
-        if(lowerChars.__contains__(password[i])):
+        if(password[i].islower()):
             advancedMask += "$l"
-        elif(upperChars.__contains__(password[i])):
+        elif((password[i]).isupper()):
             advancedMask += "$u"
-        elif(numbers.__contains__(password[i])):
+        elif((password[i]).isdigit()):
             advancedMask += "$d"
         else:
             advancedMask += "$s"
 
     return advancedMask
 
+def replaceSingleQuote(password):
+    tempPassword = ""
+    for i in range(password.__len__()):
+        if(password[i] == "'"):
+            tempPassword += "''"
+        else:
+            tempPassword += password[i]
+    return tempPassword
+
 def main():
     ##Setup connection to SQL Server
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=Malodorous;UID=sa;PWD=password')
+    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;'
+                          'DATABASE=Malodorous;UID=sa;PWD=password')
     cursor = cnxn.cursor()
     ##Open training password file
     passwordFile = open('10kMostCommon.txt', 'r')
@@ -36,12 +39,23 @@ def main():
     for password in passwordFile:
         ##Trim password
         password = password.rstrip("\n")
+        ##Escape single quotes
+        if(password.__contains__("'")):
+            password = replaceSingleQuote(password)
         ##Insert password into DB and commit
-        cursor.execute("INSERT INTO dbo.Password (Password, Length, DateAdded, Deleetified) VALUES ('" + password + "', " + str(password.__len__()) + ", '" + str(datetime.datetime.now()) + "', '" + str(False) + "');")
+        cursor.execute("INSERT INTO dbo.Password "
+                       "(Password, Length, DateAdded, Deleetified) "
+                       "VALUES ('" + password + "', " + str(password.__len__()) +
+                       ", '" + str(datetime.datetime.now()) + "', '" + str(False) + "');")
         cnxn.commit()
         ##Get the inserted password ID
         passordId = cursor.execute("SELECT @@IDENTITY").fetchone()[0]
-        passwordAdvancedMask(password)
+        ##Get password Advanced Mask
+        advancedMask = passwordAdvancedMask(password)
+        cursor.execute("INSERT INTO dbo.AdvancedMask "
+                       "(Mask, OriginalPassword) "
+                       "VALUES ('" + advancedMask + "', " + str(passordId) + ");")
+        cnxn.commit()
 
 
 if __name__ == "__main__":
