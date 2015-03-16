28d823d5af1198aa8de7ee85cd95fb2f052bda36
A-Triaca <alex.triaca@gmail.com>
2015-03-16 21:10:01 +0200

    Added Analysis  database table sql quires and method.

---
+++ Database/Create Tables Analysis.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dbo.PasswordAnalysis (
+	PasswordId INT NOT NULL IDENTITY (1, 1),
+	Password NVARCHAR(100) NOT NULL,
+	PasswordOrigin NVARCHAR(100),
+	DateAdded DATETIME2 DEFAULT GETDATE(),
+	PRIMARY KEY (PasswordId)
+);
--- MalodorousAnalyse.py
+++ MalodorousAnalyse.py
@@ -9,6 +9,19 @@ def AnalysePassword(word):
     pass
 
 
+def ResetPassword(connection, cursor):
+    ##Drop table
+    file = open("Database/Drop Tables Analysis.sql", 'r')
+    sql = " ".join(file.readlines())
+    cursor.execute(sql)
+    connection.commit()
+    ##Create table
+    file = open("Database/Create Tables Analysis.sql", 'r')
+    sql = " ".join(file.readlines())
+    cursor.execute(sql)
+    connection.commit()
+
+
 def main():
     t0 = datetime.now()
 
@@ -17,8 +30,13 @@ def main():
                           'DATABASE=Malodorous;UID=sa;PWD=password')
     cursor = connection.cursor()
 
+    ##Reset database
+    ResetPassword(connection, cursor)
+
+    ##Read in password or run through Analyse file
     password = input("Would you like to analyse a single password?\n"
                      "If yes enter the password, else leave blank")
+
     if(password == ""):
         ##Read in passwords and from Analyse folder
         analysisFolder = "Analyse"
@@ -26,16 +44,18 @@ def main():
         for (dirpath, dirnames, filenames) in walk(analysisFolder + "/"):
             fileList.extend(filenames)
             break
-
+        ##Run through folder and read in all files
         for files in fileList:
             wordFileReader = open(analysisFolder + "/" + files, 'r')
-
-        for word in files:
-            AnalysePassword(word)
+            ##Run through each file and read in each word
+            for word in files:
+                ##Analyse password
+                if(not word == ""):
+                    result = AnalysePassword(word)
 
     else:
         ##Analyse single password
-        AnalysePassword(password)
+        print("Result for " + password + " is " + AnalysePassword(password))
 
     t1 = datetime.now()
     print("Total time taken to analyse: " + str(t1-t0))
